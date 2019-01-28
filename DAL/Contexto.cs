f905fd815de25c613e9d0b889ae81e789dbe375d
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2019-01-28 08:44:44 -0400

    Creando BLL, DAL, Entities e Instalando Entity

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
---
+++ Entities/Usuarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Usuarios
+    {
+        [Key]
+
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string NombreUser { get; set; }
+        public string Clave { get; set; }
+        public string ConfirmClave { get; set; }
+        public string NivelAcceso { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            NombreUser = string.Empty;
+            Clave = string.Empty;
+            NivelAcceso = string.Empty;
+            Fecha = DateTime.Now;
+        }
+
+    }
+}
